186ecc8afcdfdc7ea2e4a3fd3cf8e4033d8971e5
PiredAcademic <99845556+PiredAcademic@users.noreply.github.com>
2022-05-13 20:57:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+//npm install ejs
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+var path = require('path');
+app.set('views', path.join(__dirname ,'/view/'));
+
+//npm install body-parser
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+//npm install consign
+var consign = require('consign');
+consign().include('controller/routes',).into(app);
+
+app.listen(8081, function(){
+    console.log("Servidor funcionando na url http://localhost:8081");
+});
